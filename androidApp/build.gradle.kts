b23a806ee146dfb168d167e6c3a3a2f7ee8e1936
Luke Powell <raistlinpowell@gmail.com>
2021-07-10 16:23:12 -0700

    Add ScoreCalculatorView to Android

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -30,8 +30,10 @@ dependencies {
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["compose_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.0-alpha06")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
 }
 
--- androidApp/src/main/java/com/dragonwellstudios/riichitoolbox/android/MainActivityCompose.kt
+++ androidApp/src/main/java/com/dragonwellstudios/riichitoolbox/android/MainActivityCompose.kt
@@ -21,37 +21,122 @@ package com.dragonwellstudios.riichitoolbox.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import com.dragonwellstudios.riichitoolbox.Greeting
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dragonwellstudios.riichitoolbox.android.ui.theme.Riichi_ToolboxTheme
+import com.dragonwellstudios.riichitoolbox.logic.Score
+import com.dragonwellstudios.riichitoolbox.logic.ScoreCalculator
 
 class MainActivityCompose : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Riichi_ToolboxTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MainView("Android")
+                    ScoreCalculatorView()
                 }
             }
         }
     }
 }
 
+class ScoreViewModel : ViewModel() {
+    private val _score = MutableLiveData(ScoreCalculator.calculate(1, 30))
+    val score: LiveData<Score> = _score
+
+    private val _han = MutableLiveData(1)
+    val han: LiveData<Int> = _han
+
+    private val _fu = MutableLiveData(1)
+    val fu: LiveData<Int> = _fu
+
+    private val _kiriageMangan = MutableLiveData(false)
+    val kiriageMangan: LiveData<Boolean> = _kiriageMangan
+
+    private val _dealer = MutableLiveData(false)
+    val dealer: LiveData<Boolean> = _dealer
+
+    fun onFuChanged(newFu: Int) {
+        _fu.value = newFu
+        recalculateScore()
+    }
+
+    fun onHanChanged(newHan: Int) {
+        _han.value = newHan
+        recalculateScore()
+    }
+
+    fun onKiriageManganChanged(newKiriageMangan: Boolean) {
+        _kiriageMangan.value = newKiriageMangan
+        recalculateScore()
+    }
+
+    fun onDealerChanged(newDealer: Boolean) {
+        _dealer.value = newDealer
+        recalculateScore()
+    }
+
+    private fun recalculateScore() {
+        _score.value = ScoreCalculator.calculate(
+            _han.value ?: 1,
+            _fu.value ?: 30,
+            _kiriageMangan.value ?: false
+        )
+    }
+}
+
 @Composable
-fun MainView(name: String) {
-    Text(text = "Hello $name!")
+fun ScoreCalculatorView(scoreViewModel: ScoreViewModel = viewModel()) {
+    val han: Int by scoreViewModel.han.observeAsState(1)
+    val fu: Int by scoreViewModel.fu.observeAsState(1)
+    val score: Score by scoreViewModel.score.observeAsState(ScoreCalculator.calculate(1, 1))
+
+    Column {
+        OutlinedTextField(
+            value = "$han",
+            onValueChange = {
+                if (it.isEmpty()) {
+                    scoreViewModel.onHanChanged(1)
+                } else {
+                    scoreViewModel.onHanChanged(it.toInt())
+                }
+            },
+            label = { Text("Han") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+        OutlinedTextField(
+            value = "$fu",
+            onValueChange = { scoreViewModel.onFuChanged(it.toInt()) },
+            label = { Text("Fu") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+        Text(text = "${score.getPayoutToDealerTsumo()}\n${score.getPayoutToDealerRon()}\n\n${score.getPayoutToNonDealerTsumo().dealer}/${score.getPayoutToNonDealerTsumo().nonDealer}\n${score.getPayoutToNonDealerRon()}")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Riichi_ToolboxTheme {
-        MainView(Greeting().greeting())
+        ScoreCalculatorView()
     }
 }
