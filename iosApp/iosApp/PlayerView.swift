c6aaf76e989493d2f945a194ad1486d144fd6c43
Luke Powell <raistlinpowell@gmail.com>
2021-07-17 21:50:45 -0700

    Add PlayerView to iOS
    - Add kanji value to Wind enum
    - Add PlayerView to match Android

---
+++ iosApp/iosApp/PlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerView.swift
+//  iosApp
+//
+//  Created by Luke Powell on 7/17/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
