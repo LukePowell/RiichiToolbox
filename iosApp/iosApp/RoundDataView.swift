1a5dad3cb3eb4799425d6e4d885f1f8ec6e8959f
Luke Powell <raistlinpowell@gmail.com>
2021-07-17 21:58:54 -0700

    Add RoundDataView/Model
    - Add starting model to RoundDataView
    - Add RoundDataView to iOS

---
+++ iosApp/iosApp/RoundDataView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundDataView.swift
+//  iosApp
+//
+//  Created by Luke Powell on 7/17/21.
+//  Copyright © 2021 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct RoundDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoundDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundDataView()
+    }
+}
